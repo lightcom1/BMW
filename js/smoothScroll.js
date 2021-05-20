c434a14723af8ed0f06a64c1346819799802c321
lightcom1 <69343253+lightcom1@users.noreply.github.com>
2021-05-20 10:38:51 +0300

    Update smoothScroll.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -4,7 +4,6 @@ smoothScrollElems.forEach(link => {
 	link.addEventListener('click', e => {
 		e.preventDefault();
 		const id = link.getAttribute('href').substring(1);
-		console.log('id: ', id);
 		document.getElementById(id).scrollIntoView({
 			behavior: 'smooth'
 		});
